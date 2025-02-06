76b149f1643ac94fccb6c572da73afa8102a58b7
JohanSora <johan.sora@mymarketlogic.com>
2025-02-06 10:59:38 -0500

    add changes

--- app/modules/orden/models.py
+++ app/modules/orden/models.py
@@ -4,14 +4,19 @@ from app.core.database import Base
 from sqlalchemy.orm import relationship
 import uuid
 
-class OrdenServicio(Base):
-    __tablename__ = "ordenes_servicio"
 
-    id_orden = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
-    id_vehiculo = Column(UUID(as_uuid=True), ForeignKey("vehiculos.id_vehiculo"))
-    id_empleado = Column(UUID(as_uuid=True), ForeignKey("empleados.id_empleado"))
+class OrdenServicio(Base):
+    __tablename__ = "orden_servicio"
+
+    id_orden = Column(UUID(as_uuid=True), primary_key=True,
+                      default=uuid.uuid4, index=True)
+    id_vehiculo = Column(UUID(as_uuid=True),
+                         ForeignKey("vehiculos.id_vehiculo"))
+    id_empleado = Column(UUID(as_uuid=True),
+                         ForeignKey("empleados.id_empleado"))
     fecha_orden = Column(Date, nullable=False)
-    estado = Column(String, nullable=False)  # pendiente, en proceso, completado
+    # pendiente, en proceso, completado
+    estado = Column(String, nullable=False)
     total_cobro = Column(DECIMAL, nullable=False)
 
     vehiculo = relationship("Vehiculo")
--- app/modules/user/models.py
+++ app/modules/user/models.py
@@ -3,20 +3,26 @@ from sqlalchemy.dialects.postgresql import UUID
 from app.core.database import Base
 import uuid
 
-class Cliente(Base):
-    __tablename__ = "clientes"
 
-    id_cliente = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
-    nombre = Column(String, nullable=False)
-    telefono = Column(String, nullable=False)
-    email = Column(String, unique=True, nullable=False)
-    cedula = Column(String, unique=True, nullable=False)
-    fecha_registro = Column(Date, nullable=False)
+class Cliente(Base):
+    __tablename__ = "cliente"
+
+    id_cliente: str = Column(UUID(as_uuid=True), primary_key=True,
+                             default=uuid.uuid4, index=True)
+    nombre: str = Column(String, nullable=False)
+    apellido: str = Column(String, nullable=False)
+    telefono: str = Column(String, nullable=False)
+    email: str = Column(String, unique=True, nullable=False)
+    document_type: str = Column(String, nullable=False)
+    cedula: int = Column(String, unique=True, nullable=False)
+    fecha_registro: str = Column(Date, nullable=False)
+
 
 class Empleado(Base):
-    __tablename__ = "empleados"
+    __tablename__ = "empleado"
 
-    id_empleado = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
+    id_empleado = Column(UUID(as_uuid=True), primary_key=True,
+                         default=uuid.uuid4, index=True)
     nombre = Column(String, nullable=False)
     apellido = Column(String, nullable=False)
     rol = Column(String, nullable=False)  # lavador, supervisor
